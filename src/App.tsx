3f7f8b80f3af00474a575752e4e23bd1e6a4221a
Yuki <yuki.marchal@edu.technobel.be>
2024-09-17 10:29:29 +0200

    Used state hook to render Pokemon's name typed in the Input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css'
+import { FormEvent, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputPokemon, setInputPokemon] = useState("");
+  const pokeRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (pokeRef.current !== null) {
+      console.log('pokeRef !== null')
+      setInputPokemon(pokeRef.current.value);
+    }
+  }
 
   return (
-    <div></div>
-  )
+    <>
+      <div className="mb-3" onSubmit={handleSubmit}>
+        <p>The pokemon you selected : {inputPokemon.toUpperCase()}</p>
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Pokemon's name"
+            ref={pokeRef}
+          />
+          <button className="btn btn-primary my-2" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
